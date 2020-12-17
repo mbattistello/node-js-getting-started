3ca41c12142651dbf070d3e0e8d49cff2fc5c380
Marc Battistello <marc.battistello@gmail.com>
2020-12-17 17:15:17 -0600

    init commit

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+var contacts = { "contacts" : [ 
+                { "_id" : 1, "name": "Marc", "email": "marc.battistello@gmail.com", "phone": { "mobile": "518-951-6809" } },
+                 { "_id" : 2, "name": "Marc2", "email": "marc.battistello2@gmail.com", "phone": { "mobile": "2518-951-6809" } } 
+                ] };
+
+app.get("/api/contacts", function(req, res) {
+
+  res.status(200).json( contacts );
+
+});
